201f7772e9757da3489a9e70a64cf40079776ad9
DripSwag <mimirani33@gmail.com>
2023-11-10 22:11:21 +0000

    Added more

--- src/components/FlipCalendar.tsx
+++ src/components/FlipCalendar.tsx
@@ -5,8 +5,11 @@ interface Params {
 export default function FlipCalendar({text}: Params) {
   return (
     <div className="flex flex-col gap-4 items-center">
-      <div className="h-full w-full bg-gradient-to-b from-[#2c2c44] to-quaternary">
-        <h3 className="">5</h3>
+      <div className="w-full max-w-[125px] aspect-square bg-background flex justify-center items-center dropshadow relative">
+        <h3 className="text-6xl text-secondary font-bold font-redhat z-10">08</h3>
+        <span className="lighthalf"></span>
+        <span className="semicircle-left"></span>
+        <span className="semicircle-right"></span>
       </div>
       <h2 className="font-redhat text-primary text-[14px] tracking-[0.3rem]">{text}</h2>
     </div>
