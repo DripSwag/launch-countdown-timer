8a6a0b6f50138606a984cda3789c2d07321041ae
DripSwag <mimirani33@gmail.com>
2023-11-10 16:32:14 +0000

    Added background

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import background from '../assets/pattern-hills.svg'
+import stars from '../assets/bg-stars.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className='h-screen w-screen bg-gradient-to-b from-[#1e1e28] to-[#251D2C] to-70%'>
+      <div className='max-w-[1440px] w-1/2 h-1/2 bg-white relative left-1/2 -translate-x-1/2 top-1/3 -translate-y-1/3 z-50 flex items-center flex-col gap-16'>
+        <h1>We&apos;re launching soon</h1>
+        <div className='grid grid-cols-4 w-full h-full'></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <img src={stars} className='w-full h-full fixed z-10'></img>
+      <img src={background} className='w-full fixed bottom-0 z-0'></img>
+    </main>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        background: 'hsl(236, 21%, 26%)',
+        primary: 'hsl(237, 18%, 59%)',
+        secondary: 'hsl(345, 95%, 68%)',
+        tertiary: 'hsl(235, 16%, 14%)',
+        'tertiary-dark': 'hsl(234, 17%, 12%)',
+        text: 'hsl(0, 0%, 100%)',
+      },
+    },
+  },
+  plugins: [],
+}
